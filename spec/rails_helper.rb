c982079b57a12202ae581e2e68af8c6af1e7a44e
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2020-12-03 07:04:19 -0300

    devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,10 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+
+# Add these after require 'rspec/rails'
+require 'devise'
+require_relative 'support/controller_macros'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -56,7 +60,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include FactoryBot::Syntax::Methods
+  config.extend ControllerMacros, :type => :controller
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
