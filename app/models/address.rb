ba977e4c88c67ff0183150986e1db2be323f4537
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2020-12-04 11:14:21 -0300

    validates and activestorage

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :zip, :street, :complement, :neighborhood, :city, :uf, presence: true
+  validates :zip, uniqueness: true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+# Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -29,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
