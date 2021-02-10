c400f2ea2ed2ac79083134284dbb2eeba4e5aa07
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-02-10 20:34:57 -0300

    clean code and refact address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,6 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /addresses
-  # GET /addresses.json
 
   def index
     addresses = Address.all
@@ -16,21 +14,15 @@ class AddressesController < ApplicationController
     @cities_rank = rank_cities.uniq
   end
 
-  # GET /addresses/1
-  # GET /addresses/1.json
   def show
   end
   
   def fill
     address_check =  Address.find_by(user: current_user)
     cep = params[:cep_service]
-    
+
     if address_check.present?
-      if cep.blank? 
-        redirect_to edit_address_path(address_check), alert: "Voce não preencheu o cep!"
-      else
-        redirect_to edit_address_path(address_check, cep_service: params[:cep_service])
-      end
+      cep.blank? ? ( redirect_to edit_address_path(address_check), alert: "Voce não preencheu o cep!" ) : (redirect_to edit_address_path(address_check, cep_service: params[:cep_service]))
     else
       service = CepService.new(cep)
       
@@ -41,6 +33,7 @@ class AddressesController < ApplicationController
       else
         flash[:alert] = service.message
       end
+      
       if cep.blank?
         redirect_to new_address_path, alert: "Voce não preencheu o cep!"
       else
@@ -49,9 +42,9 @@ class AddressesController < ApplicationController
     end
   end
 
-  # GET /addresses/new
   def new
     address =  Address.find_by(user: current_user)
+    
     if address.present?
       redirect_to edit_address_path(address)
     else 
@@ -59,7 +52,6 @@ class AddressesController < ApplicationController
     end
   end
 
-  # GET /addresses/1/edit
   def edit
     if params[:cep_service].present?
       service = CepService.new(params[:cep_service])
@@ -80,10 +72,7 @@ class AddressesController < ApplicationController
     end
   end
 
-  # POST /addresses
-  # POST /addresses.json
   def create
- 
     @address = Address.new(address_params.merge(user: current_user))
 
     respond_to do |format|
@@ -97,8 +86,6 @@ class AddressesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /addresses/1
-  # PATCH/PUT /addresses/1.json
   def update
     respond_to do |format|
       if @address.update(address_params)
@@ -111,23 +98,11 @@ class AddressesController < ApplicationController
     end
   end
 
-  # DELETE /addresses/1
-  # DELETE /addresses/1.json
-  def destroy
-    @address.destroy
-    respond_to do |format|
-      format.html { redirect_to addresses_url, notice: 'Address was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_address
       @address = Address.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def address_params
       params.require(:address).permit(:zip, :street, :complement, :neighborhood, :city, :uf, :ibge_code, :user_id)
     end
