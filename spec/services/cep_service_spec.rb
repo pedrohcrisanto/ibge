2c3b7ca2cc84452b6f485b7c8b0c253c3a3a5a04
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-02-10 17:32:23 -0300

    change service test

--- spec/services/cep_service_spec.rb
+++ spec/services/cep_service_spec.rb
@@ -8,25 +8,28 @@ describe CepService do
       subject { instance.find }
 
       let(:cep) { "58101245" }
-    
+
       it "returns cep data" do
-          expect(subject.zip).to eq "58101-245"
-          expect(subject.street).to eq "Rua Rodrigo Santiago de Brito Pereira"
-          expect(subject.complement).to eq ""
-          expect(subject.neighborhood).to eq "Areia Dourada"
-          expect(subject.city).to eq "Cabedelo"
-          expect(subject.uf).to eq "PB"
-          expect(subject.ibge_code).to eq "2503209"
+        expect(subject).to be_truthy
+        expect(instance.message).to "Seus dados foram preenchidos corretamente."
+        expect(instance.data.zip).to eq "58101-245"
+        expect(instance.data.street).to eq "Rua Rodrigo Santiago de Brito Pereira"
+        expect(instance.data.complement).to eq ""
+        expect(instance.data.neighborhood).to eq "Areia Dourada"
+        expect(instance.data.city).to eq "Cabedelo"
+        expect(instance.data.uf).to eq "PB"
+        expect(instance.data.ibge_code).to eq "2503209"
       end
     end
 
     context "with invalid cep" do
       subject { instance.find }
 
-      let(:cep) { "123" }
+      let(:cep) { "58101" }
 
       it "return invalid message" do
-          expect(subject).to eq "Algo deu errado!"
+        expect(subject).to be_falsey
+        expect(instance.message).to eq "CEP Inválido!"
       end
     end
   end
